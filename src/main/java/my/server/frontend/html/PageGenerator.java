674391e3974694ba441b13d2552c9fa36f4852ac
Baburin Sergey <sbasba777@hotmail.com>
2015-11-20 17:47:59 +0300

    Refactor PageGenerator with FreeMarker

--- src/main/java/my/server/frontend/html/PageGenerator.java
+++ src/main/java/my/server/frontend/html/PageGenerator.java
@@ -34,55 +34,40 @@ public class PageGenerator {
         }
     }
 
-    private static String staticPage(String head, String body, int sessionId) {
+    private static String getPage(Map<String, Object> dataModel) {
         Writer stream = new StringWriter();
-        Map<String, Object> data = new HashMap<>();
-        data.put("body", formPost(body, sessionId));
-        data.put("refreshable", false);
         try {
             Template template = CFG.getTemplate(CORE_PAGE);
-            template.process(data, stream);
+            template.process(dataModel, stream);
         } catch (IOException | TemplateException e) {
             e.printStackTrace();
         }
         return stream.toString();
     }
 
-    private static String refreshablePage(String head, String body, int sessionId) {
-        Writer stream = new StringWriter();
-        Map<String, Object> data = new HashMap<>();
-        data.put("body", formPost(body, sessionId));
-        data.put("refreshable", true);
-        try {
-            Template template = CFG.getTemplate(CORE_PAGE);
-            template.process(data, stream);
-        } catch (IOException | TemplateException e) {
-            e.printStackTrace();
-        }
-        return stream.toString();
+    private static Map<String, Object> getDataModel(int sessionId, String name, Integer userId, Long duration) {
+        Map<String, Object> dataModel = new HashMap<>();
+        dataModel.put("sessionId", String.format("%04d", sessionId));
+        dataModel.put("name", name);
+        dataModel.put("userId", userId);
+        dataModel.put("duration",
+                duration != null ? TimeHelper.formatTime(duration) : null);
+        return dataModel;
     }
 
-    private static String formPost(String text, int sessionId) {
-        return "<form method=\"post\"> " +
-                    text +
-                    "<input type=\"hidden\" name=\"sessionId\" value=\"" + sessionId +"\">" +
-                "</form>";
+    private static Map<String, Object> getDataModel(int sessionId, String name, Integer userId) {
+        return getDataModel(sessionId, name, userId, null);
     }
 
-    private static String header(int sessionId, String name, Integer userId, Long duration) {
-        return String.format("<hr>[Session ID: <b>%04d</b>] %s %s %s<hr>",
-                        sessionId,
-                        name != null ? "[User name: <b>" + name + "</b>]" : "",
-                        userId != null ? "[User ID: <b>" + userId + "</b>]" : "",
-                        duration != null
-                                ? "[Duration: " +
-                                    "<b>" +
-                                        TimeHelper.formatTime(duration) +
-                                    "</b>]"
-                                : ""
-                );
+    private static Map<String, Object> getDataModel(int sessionId, String name) {
+        return getDataModel(sessionId, name, null, null);
     }
 
+    private static Map<String, Object> getDataModel(int sessionId) {
+        return getDataModel(sessionId, null, null, null);
+    }
+
+    //TODO
     private static String highScoresTable(UserSession userSession, List<Results> highScores) {
         String thisSessionName = userSession.getUserName();
         int thisSessionAttempts = userSession.getGameReplica().getAttempts();
@@ -118,38 +103,43 @@ public class PageGenerator {
         return table.toString();
     }
 
+    //TODO
     public static String getRequestNamePage(UserSession userSession) {
         int sessionId = userSession.getSessionId();
-        return staticPage("",
-                header(sessionId, null, null, null) +
-                    "<p>Enter your name: " +
-                        "<input name=\"userName\" required autofocus placeholder=\"Your name\">" +
-                        "<input type=\"submit\" value=\"Send\">" +
-                    "</p>",
-                sessionId
-        );
+        Map<String, Object> dataModel = getDataModel(sessionId);
+        dataModel.put("requestName", true);
+
+        dataModel.put("body", "");
+        return getPage(dataModel);
     }
 
     public static String getWaitAuthorizationPage(UserSession userSession) {
         int sessionId = userSession.getSessionId();
         String name = userSession.getUserName();
-        return refreshablePage("",
-                header(sessionId, name, null, null) +
-                        "<p><b>Wait for authorization!</b></p>",
-                sessionId
-        );
+
+        Map<String, Object> dataModel = getDataModel(sessionId, name);
+        dataModel.put("refreshable", true);
+
+        String body = "<p><b>Wait for authorization!</b></p>";
+
+        dataModel.put("body", body);
+        return getPage(dataModel);
+
     }
 
     public static String getAuthorizationOKPage(UserSession userSession) {
         int sessionId = userSession.getSessionId();
         String name = userSession.getUserName();
         int userId = userSession.getUserId();
-        return staticPage("",
-                header(sessionId, name, userId, null) +
-                    "<p><b>Authorization OK!</b></p>" +
-                    "<hr><input type=\"submit\" name=\"startGame\" value=\"Start game!\">",
-                sessionId
-        );
+
+        Map<String, Object> dataModel = getDataModel(sessionId, name, userId);
+
+        String body = "<p><b>Authorization OK!</b></p>" +
+                "<hr><input type=\"submit\" name=\"startGame\" value=\"Start game!\">";
+
+        dataModel.put("body", body);
+        return getPage(dataModel);
+
     }
 
     public static String getGamePage(UserSession userSession) {
@@ -161,20 +151,23 @@ public class PageGenerator {
         int upper = userSession.getGameReplica().getUpperBound();
         String message = userSession.getGameReplica().getMessage();
         int attempts = userSession.getGameReplica().getAttempts();
-        return staticPage("",
-                header(sessionId, name, userId, duration) +
-                    "<p>Enter the number between <b>" + lower +"</b> and <b>" + upper + "</b>:</p>" +
-                    "<p>" +
-                        String.format("<input type=\"number\" name=\"turn\" min=\"%d\" max=\"%d\" step=\"1\" required autofocus placeholder=\"%d ... %d\">",
-                                lower, upper, lower, upper) +
-                        "<input type=\"submit\" value=\"Play!\">" +
-                    "</p>" +
-                    String.format("<hr>[Attempts: <b>%d</b>] %s",
-                            attempts,
-                            message != null ? "[Goal: <b>" + message + "</b>]" : ""
-                    ),
-                sessionId
-        );
+
+        Map<String, Object> dataModel = getDataModel(sessionId, name, userId, duration);
+
+        String body = "<p>Enter the number between <b>" + lower +"</b> and <b>" + upper + "</b>:</p>" +
+                "<p>" +
+                String.format("<input type=\"number\" name=\"turn\" min=\"%d\" max=\"%d\" step=\"1\" required autofocus placeholder=\"%d ... %d\">",
+                        lower, upper, lower, upper) +
+                "<input type=\"submit\" value=\"Play!\">" +
+                "</p>" +
+                String.format("<hr>[Attempts: <b>%d</b>] %s",
+                        attempts,
+                        message != null ? "[Goal: <b>" + message + "</b>]" : ""
+                );
+
+        dataModel.put("body", body);
+        return getPage(dataModel);
+
     }
 
     public static String getGameOverPage(UserSession userSession, List<Results> highScores) {
@@ -184,13 +177,16 @@ public class PageGenerator {
         long duration = userSession.getGameSessionDuration();
         int goal = userSession.getGameReplica().getLastTurn();
         int attempts = userSession.getGameReplica().getAttempts();
-        return staticPage("",
-                header(sessionId, name, userId, duration) +
-                        "<p><b>Game over!</b></p>" +
-                        String.format("<p>You found <b>%d</b> with <b>%d</b> attempts.</p>", goal, attempts) +
-                        "<hr>" + highScoresTable(userSession, highScores) +
-                        "<hr><input type=\"submit\" name=\"startGame\" value=\"Play again!\">",
-                sessionId
-        );
+
+        Map<String, Object> dataModel = getDataModel(sessionId, name, userId, duration);
+
+        String body = "<p><b>Game over!</b></p>" +
+                String.format("<p>You found <b>%d</b> with <b>%d</b> attempts.</p>", goal, attempts) +
+                "<hr>" + highScoresTable(userSession, highScores) +
+                "<hr><input type=\"submit\" name=\"startGame\" value=\"Play again!\">";
+
+        dataModel.put("body", body);
+        return getPage(dataModel);
+
     }
 }
